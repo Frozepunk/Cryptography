94890357fb31c61ae179f25246ebd81d4f80fbab
Frozepunk <smg2750935@gmail.com>
2024-09-02 21:49:25 +0530

    combining both the codes

--- caser-cipher-decryption.py
+++ caser-cipher-decryption.py
@@ -1,39 +1,39 @@
-# result = []
-# text = input("Enter text\n ")
-# key = int(input("Enter the key value \n"))
-# for char in text:
-#     if char.islower():
-#         base = ord('a')
-#         res = chr((ord(char) - base - key) % 26 + base)
-#         result.append(res)
-#     elif char.isupper():
-#         base = ord('A')
-#         res = chr((ord(char) - base - key) % 26 + base)
-#         result.append(res)
-#     elif char.isdigit():
-#         base = ord('0')
-#         res = chr((ord(char) - base - key) % 10 + base)
-#         result.append(res)
-#     else:
-#         result.append(char)
-# print(''.join(result))
-def caeser_cipher():
-    result = []
-    text = input("Enter text\n ")
-    key = int(input("Enter the key value \n"))
-    for char in text:
-        if char.islower():
-            base = ord('a')
-            res = chr((ord(char) - base - key) % 26 + base)
-            result.append(res)
-        elif char.isupper():
-            base = ord('A')
-            res = chr((ord(char) - base - key) % 26 + base)
-            result.append(res)
-        elif char.isdigit():
-            base = ord('0')
-            res = chr((ord(char) - base - key) % 10 + base)
-            result.append(res)
-        else:
-            result.append(char)
-    print(''.join(result))
+result = []
+text = input("Enter text\n ")
+key = int(input("Enter the key value \n"))
+for char in text:
+    if char.islower():
+        base = ord('a')
+        res = chr((ord(char) - base - key) % 26 + base)
+        result.append(res)
+    elif char.isupper():
+        base = ord('A')
+        res = chr((ord(char) - base - key) % 26 + base)
+        result.append(res)
+    elif char.isdigit():
+        base = ord('0')
+        res = chr((ord(char) - base - key) % 10 + base)
+        result.append(res)
+    else:
+        result.append(char)
+print(''.join(result))
+# def caeser_cipher():
+#     result = []
+#     text = input("Enter text\n ")
+#     key = int(input("Enter the key value \n"))
+#     for char in text:
+#         if char.islower():
+#             base = ord('a')
+#             res = chr((ord(char) - base - key) % 26 + base)
+#             result.append(res)
+#         elif char.isupper():
+#             base = ord('A')
+#             res = chr((ord(char) - base - key) % 26 + base)
+#             result.append(res)
+#         elif char.isdigit():
+#             base = ord('0')
+#             res = chr((ord(char) - base - key) % 10 + base)
+#             result.append(res)
+#         else:
+#             result.append(char)
+#     print(''.join(result))
---
+++ caser-cipher.py
@@ -0,0 +1,46 @@
+def caesar_cipher():
+    text = input("Enter text: ")
+    key = int(input("Enter the key value: "))
+
+    # Encryption
+    encrypted = []
+    for char in text:
+        if char.islower():
+            base = ord('a')
+            res = chr((ord(char) - base + key) % 26 + base)
+            encrypted.append(res)
+        elif char.isupper():
+            base = ord('A')
+            res = chr((ord(char) - base + key) % 26 + base)
+            encrypted.append(res)
+        elif char.isdigit():
+            base = ord('0')
+            res = chr((ord(char) - base + key) % 10 + base)
+            encrypted.append(res)
+        else:
+            encrypted.append(char)
+
+    encrypted_text = ''.join(encrypted)
+    print("Encrypted text-->", encrypted_text)
+
+    # Decryption
+    decrypted = []
+    for char in encrypted_text:
+        if char.islower():
+            base = ord('a')
+            res = chr((ord(char) - base - key) % 26 + base)
+            decrypted.append(res)
+        elif char.isupper():
+            base = ord('A')
+            res = chr((ord(char) - base - key) % 26 + base)
+            decrypted.append(res)
+        elif char.isdigit():
+            base = ord('0')
+            res = chr((ord(char) - base - key) % 10 + base)
+            decrypted.append(res)
+        else:
+            decrypted.append(char)
+
+    print("Decrypted text-->", ''.join(decrypted))
+
+caesar_cipher()
