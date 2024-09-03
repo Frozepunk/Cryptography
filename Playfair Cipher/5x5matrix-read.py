af3c1ffc981e7552ce79452ee1ffb8753cf07331
Frozepunk <smg2750935@gmail.com>
2024-09-03 12:30:46 +0530

    reading both char and numbers

--- Playfair Cipher/5x5matrix-read.py
+++ Playfair Cipher/5x5matrix-read.py
@@ -6,8 +6,7 @@ print("Enter the entries row-wise:")
 for i in range(rows):          
     row = [] 
     for j in range(columns):     
-        
-        element = int(input(f"Enter element for row {i+1}, column {j+1}: "))
+        element = input(f"Enter element for row {i+1}, column {j+1}: ")
         row.append(element)
     matrix.append(row)
 print("\nThe resulting matrix is:")
