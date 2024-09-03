88722929e45cda7880026d17cb7ae27258caf32c
Frozepunk <smg2750935@gmail.com>
2024-09-03 11:41:48 +0530

    creating directories and managing the files and creating a matrix to read the data from the user

---
+++ Playfair Cipher/5x5matrix-read.py
@@ -0,0 +1,17 @@
+rows = int(input("Enter the number of rows: "))
+columns = int(input("Enter the number of columns: "))
+matrix = []
+print("Enter the entries row-wise:")
+
+for i in range(rows):          
+    row = [] 
+    for j in range(columns):     
+        
+        element = int(input(f"Enter element for row {i+1}, column {j+1}: "))
+        row.append(element)
+    matrix.append(row)
+print("\nThe resulting matrix is:")
+for i in range(rows):
+    for j in range(columns):
+        print(matrix[i][j], end=" ")
+    print()
