044b2f342903a4322a5069ceeae3502ea48c228c
Frozepunk <smg2750935@gmail.com>
2024-09-05 08:59:33 +0530

    ntg

--- Playfair Cipher/5x5matrix-read.py
+++ Playfair Cipher/5x5matrix-read.py
@@ -1,15 +1,14 @@
-rows = int(input("Enter the number of rows: "))
-columns = int(input("Enter the number of columns: "))
+rows = 5
+columns = 5
 matrix = []
-print("Enter the entries row-wise:")
-
+print("Enter the entries row-wise -->")
 for i in range(rows):          
     row = [] 
     for j in range(columns):     
-        element = input(f"Enter element for row {i+1}, column {j+1}: ")
+        element = input(f"Enter element for row {i+1}, column {j+1} --> ")
         row.append(element)
     matrix.append(row)
-print("\nThe resulting matrix is:")
+print("\nThe resulting matrix is \n")
 for i in range(rows):
     for j in range(columns):
         print(matrix[i][j], end=" ")
